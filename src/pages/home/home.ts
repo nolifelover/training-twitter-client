28b2fefb8992496be8a586098148df18c0adc88c
todsaporn sangboon <nolifelover@gmail.com>
2018-07-22 13:47:49 +0700

    add tab search

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  apiEndpoint = "http://localhost:3000/api/tweets"
+  tweets = []
+  constructor(public navCtrl: NavController, private http: HttpClient) {
+  
+  }
 
-  constructor(public navCtrl: NavController) {
+  ngOnInit(){
+    this.getTweets();
+  }
 
+  getTweets(){
+    this.http.get(this.apiEndpoint).subscribe((result:any)=>{
+      console.log(result);
+      if(result.status == 200){
+        this.tweets = result.datas
+      }
+    })
   }
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { SearchPage } from '../search/search';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabHome = HomePage;
+  tabSearch = SearchPage;
+  tabAbout = AboutPage;
 
   constructor() {
 
